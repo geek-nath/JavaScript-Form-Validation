d2efac1d06c18c419cacd70612b48c3a980a1c9f
geek-nath <nath.codes@gmail.com>
2023-03-15 13:41:43 -0700

    html & css for log in complete, working on js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// form validation data for log in
+
+const userDetails = {
+    username: "admin",
+    password: "admin001"
+};
+
+const userName = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+
+// check if user input === with userDetails 
