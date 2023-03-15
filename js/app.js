1c256440fbe7caacfb77ce07896be81f6dcb43a0
geek-nath <nath.codes@gmail.com>
2023-03-15 14:33:40 -0700

    submit form button bug not fixed

--- js/app.js
+++ js/app.js
@@ -5,7 +5,16 @@ const userDetails = {
     password: "admin001"
 };
 
-const userName = document.getElementById('username').value;
-const password = document.getElementById('password').value;
-
 // check if user input === with userDetails 
+
+const logInFormEl = document.getElementById('password');
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const userName = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    console.log('hello world')
+});
