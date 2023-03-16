e5b94d94a25b5b0dbb6cf4f43384f262360d2995
geek-nath <nath.codes@gmail.com>
2023-03-16 06:08:42 -0700

    working on show password function

--- js/app.js
+++ js/app.js
@@ -9,12 +9,45 @@ const userDetails = {
 
 const logInFormEl = document.getElementById('password');
 const submitButton = document.getElementById('submitButton');
+const errorMessage = document.getElementById('errorMessage');
 
-submitButton.addEventListener('submit', (e)=> {
+submitButton.addEventListener('click', (e)=> {
     e.preventDefault();
 
     const userName = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    console.log('hello world')
+    if((userName === userDetails.username) && (password === userDetails.password)) {
+        console.log('Login successfull');
+        errorMessage.classList.add('text-success');
+        errorMessage.innerHTML = "Login Successful"
+    } 
+    
+    else if((userName === userDetails.username) && (password !== userDetails.password)) {
+        console.log('Incorrect password');
+        errorMessage.classList.add('text-danger');
+        errorMessage.innerHTML = "Incorrect password"
+    } 
+    
+    else if((password === userDetails.password) && (userName !== userDetails.username)) {
+        console.log('Username not found');
+        errorMessage.classList.add('text-danger');
+        errorMessage.innerHTML = "Username not found"
+    }
+
+    else if((userName.length === 0) && (password.length === 0)) {
+        console.log('Please enter your login details');
+        errorMessage.classList.add('text-danger');
+        errorMessage.innerHTML = "Please enter your login details."
+    }
+});
+
+// show password toggle
+
+const showPasswordToggle = document.getElementById('showPasswordToggle');
+
+showPasswordToggle.addEventListener('click', ()=> {
+    const password = document.getElementById('password');
+
+    
 });
